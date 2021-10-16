429bc0840c84d6c34bb0cef8510a0be9cb167f9a
Anurag Kumar <anurags62@yahoo.com>
2021-10-16 17:19:01 +0530

    update

--- SpringMvc3ByBryan/01_springmvc3-webapp-fitness-tracker/src/main/java/com/anurag/MinutesController.java
+++ SpringMvc3ByBryan/01_springmvc3-webapp-fitness-tracker/src/main/java/com/anurag/MinutesController.java
@@ -1,15 +1,15 @@
 package com.anurag;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MinutesController {
 
-    @RequestMapping("/addMinutes")
-    public String addMinutes(Model model) {
-        model.addAttribute("");
+    @RequestMapping(value = "/addMinutes")
+    public String submitMinutes(@ModelAttribute("exercise") Excercise excercise) {
+        System.out.println(excercise);
         return "addMinutes";
     }
 }
---
+++ SpringMvc3ByBryan/03_springmvc3-webapp-fitness-tracker-host-static-files/Readme.md
@@ -0,0 +1,5 @@
+1.host static files or accessing static views:
+**
+<mvc:annotation-driven/>
+<mvc:resources mapping="/pdfs/**" location="/pdfs/"/>
+**
