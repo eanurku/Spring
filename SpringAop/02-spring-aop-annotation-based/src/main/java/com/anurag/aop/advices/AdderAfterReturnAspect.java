1e98b4753e80d5509a1822152a03a0c76d922e1b
Anurag Kumar <anurags62@yahoo.com>
2021-11-14 11:10:44 +0530

    update

---
+++ SpringAop/02-spring-aop-annotation-based/src/main/java/com/anurag/aop/advices/AdderAfterReturnAspect.java
@@ -0,0 +1,19 @@
+package com.anurag.aop.advices;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AdderAfterReturnAspect {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    public void afterReturn(Object returnValue) {
+        LOGGER.info("here is the return value ={}", returnValue);
+
+    }
+
+}
